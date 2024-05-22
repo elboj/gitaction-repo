7971f4b2b676e24396482414581fce943d60266d
elboj <adaja.tobi1@gmail.com>
2024-05-22 16:17:59 +0100

    testing remote repo structure

---
+++ terraform/template/main.tf
@@ -0,0 +1,15 @@
+module "azurerm_linux_virtual_machine" {
+  # source = "../modules/linux-virtual-machine"
+  source = "git::https://github.com/elboj/Terraform-Modules.git//modules/linux-virtual-machine" 
+  virtual_network_name = "vnt-uks-gen-devy001"
+  resource_group_name = "rg-uks-gen-defv001"
+  location = "UK South"
+  subnet_name = "default"
+  vm_public_ip = "nic-uks-vm-dev001"
+  nic_name = "ip_config"
+  ip_config = "pip-uks-gen-dev001"
+  vm_name = "vm-uks-gen-dev001"
+  vm_size = "Standard_DS1_v2"
+  admin_username = "elboj_admin"
+  admin_password = ""
+}
